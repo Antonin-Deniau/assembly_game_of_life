e45edf399ef787a78c345a2e9c2eedd30e0ea09f
Antonin DENIAU <antonin.deniau@protonmail.com>
2017-02-19 10:43:39 -0500

    gol

--- os.asm
+++ os.asm
@@ -1,23 +1,65 @@
 BITS 64
 
 main:
-  mov rax, welcome
-  int 0x1
+  push rbx
+  mov rbx, rsp
 
-  mov rax, buffer
-  int 0x2
+  call init_screen
 
-  mov rdx, 0
-  loop:
-    mov rax, hello
-    int 0x1
-    add rdx, 1
-    cmp rdx, 10
-    jle loop
+  mov byte [screen + 5], "A"
+
+  call refresh_screen
+  
+  call sleep
+
+  call exit
+
+  pop rbx
+  ret
+
+exit:
+  push rbx
+  mov rbx, rsp
 
   int 0xFF
 
-strings:
-  welcome db "Whats your name ?", 0
-  hello db "Hello "
-  buffer db ""
+  pop rbx
+  ret
+
+sleep:
+  push rbx
+  mov rbx, rsp
+
+  mov rax, 2
+  int 0x2
+
+  pop rbx
+  ret
+
+refresh_screen:
+  push rbx
+  mov rbx, rsp
+
+  mov rax, screen
+  int 0x1
+
+  pop rbx
+  ret
+
+init_screen:
+  push rbx
+  mov rbx, rsp
+
+  mov r12, 0
+  loop:
+    mov byte [screen + r12], 0x20
+
+    inc r12
+    cmp r12, 5000
+    jle loop
+
+  pop rbx
+  ret
+
+data:
+  screen db ""
