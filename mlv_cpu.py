8bca388a01dd055c26a2ef839862ad31fb39e36c
DENIAU Antonin <antonin.deniau@gmail.com>
2017-02-23 14:07:45 +0100

    working_ui_putain

--- mlv_cpu.py
+++ mlv_cpu.py
@@ -6,26 +6,26 @@ from unicorn.x86_const import *
 
 ADDRESS = 0x0
 
-class Cpu(Thread):
-  def __init__(self, devices, binary):
-    Thread.__init__(self)
+def run(hook_intr, binary):
+  mu = Uc(UC_ARCH_X86, UC_MODE_64)
 
+  mu.mem_map(ADDRESS, 2 * 1024 * 1024)
+  mu.reg_write(UC_X86_REG_RSP, ADDRESS + 0x200000)
+  mu.reg_write(UC_X86_REG_RBP, ADDRESS + 0x200000)
+  mu.hook_add(UC_HOOK_INTR, hook_intr)
+  mu.mem_write(ADDRESS, binary)
+
+  mu.emu_start(ADDRESS, ADDRESS + len(binary))
+
+class Cpu():#Thread):
+  def __init__(self, devices, binary):
+    #Thread.__init__(self)
     self.devices = devices
     self.binary = binary
 
-    try:
-      self.mu = Uc(UC_ARCH_X86, UC_MODE_64)
-      self.mu.mem_map(ADDRESS, 2 * 1024 * 1024)
-      self.mu.reg_write(UC_X86_REG_RSP, ADDRESS + 0x200000)
-      self.mu.reg_write(UC_X86_REG_RBP, ADDRESS + 0x200000)
-      self.mu.hook_add(UC_HOOK_INTR, self.hook_intr)
-      self.mu.mem_write(ADDRESS, self.binary)
-
-    except UcError as e:
-      print("ERROR: %s" % e)
-
-  def run(self):
-    self.mu.emu_start(ADDRESS, ADDRESS + len(self.binary))
+  def start(self):
+    t = Thread(target=run, args=(self.hook_intr, self.binary))
+    t.start()
 
   def hook_intr(self, uc, intno, user_data):
     for device in self.devices:
